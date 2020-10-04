d903eefdd0f329ef1720b2e6f75b85cc642ced1e
sadnessOjisan <sadness.ojisan@gmail.com>
2020-10-04 23:44:17 +0900

    via github url (#1)
    * access url
    
    * chore get data from url
    
    * fix readme

--- cmd.ml
+++ cmd.ml
@@ -1,6 +1,8 @@
 open Core
 open Yojson.Basic.Util
 open Out_channel
+open Lwt
+open Cohttp_lwt_unix
 
 let js = Yojson.Basic.from_channel (In_channel.create "package.json")
 
@@ -12,12 +14,19 @@ let rec print_list = function
       print_list l
 
 let () =
-  let json2 = Yojson.Basic.from_file "package.json" in
-      (* Test that the two values are the same *)
-      let dependencies = json2 |> member "devDependencies" |> keys in
-      print_string "npm i -D ";
-      print_list dependencies;
-      print_endline "";
-      let dependencies = json2 |> member "dependencies" |> keys in
-      print_string "npm i ";
-      print_list dependencies
+  let cmds = Sys.get_argv () in
+  let url = cmds.(1) in
+  let body =
+    Client.get (Uri.of_string url) >>= fun (_, body) ->
+    body |> Cohttp_lwt.Body.to_string >|= fun body -> body
+  in
+  let json = Lwt_main.run body in
+  let json_object = Yojson.Basic.from_string json in
+  (* Test that the two values are the same *)
+  let dependencies = json_object |> member "devDependencies" |> keys in
+  print_string "npm i -D ";
+  print_list dependencies;
+  print_endline "";
+  let dependencies = json_object |> member "dependencies" |> keys in
+  print_string "npm i ";
+  print_list dependencies
