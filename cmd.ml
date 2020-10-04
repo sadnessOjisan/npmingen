11cd49b2149b7a3018a1f8b827e884bd61ca47d2
sadnessOjisan <sadness.ojisan@gmail.com>
2020-10-04 23:41:14 +0900

    chore get data from url

--- cmd.ml
+++ cmd.ml
@@ -13,21 +13,20 @@ let rec print_list = function
       print_string " ";
       print_list l
 
-let body =
-  Client.get
-    (Uri.of_string
-       "https://raw.githubusercontent.com/sadnessOjisan/npmingen/master/package.json")
-  >>= fun (_, body) ->
-  body |> Cohttp_lwt.Body.to_string >|= fun body -> body
-
 let () =
-  let body = Lwt_main.run body in
-  let json2 = Yojson.Basic.from_string body in
+  let cmds = Sys.get_argv () in
+  let url = cmds.(1) in
+  let body =
+    Client.get (Uri.of_string url) >>= fun (_, body) ->
+    body |> Cohttp_lwt.Body.to_string >|= fun body -> body
+  in
+  let json = Lwt_main.run body in
+  let json_object = Yojson.Basic.from_string json in
   (* Test that the two values are the same *)
-  let dependencies = json2 |> member "devDependencies" |> keys in
+  let dependencies = json_object |> member "devDependencies" |> keys in
   print_string "npm i -D ";
   print_list dependencies;
   print_endline "";
-  let dependencies = json2 |> member "dependencies" |> keys in
+  let dependencies = json_object |> member "dependencies" |> keys in
   print_string "npm i ";
   print_list dependencies
